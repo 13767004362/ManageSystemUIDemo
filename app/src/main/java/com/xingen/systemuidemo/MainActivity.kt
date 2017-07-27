bec5ed1824f9e803ba29684e9a85bfddee8edbab
xingen <13767004362@163.com>
2017-07-27 19:34:28 +0800

    Initial commit

---
+++ app/src/main/java/com/xingen/systemuidemo/MainActivity.kt
@@ -0,0 +1,97 @@
+package com.xingen.systemuidemo
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import android.view.GestureDetector
+import android.view.MotionEvent
+import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+class MainActivity : AppCompatActivity() ,View.OnClickListener{
+    private var tag=MainActivity::class.java.simpleName
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        initView()
+        setSystemUIChangeListener()
+        setGestureHandler()
+    }
+
+    /**
+     * 初始化控件
+     */
+    fun  initView(){
+        main_stickyStyle.setOnClickListener (this)
+        main_normalStyle.setOnClickListener(this)
+        main_dimStyle.setOnClickListener(this)
+    }
+
+    /**
+     * 监听System bar异步改变，来控制actionbar。
+     */
+    fun setSystemUIChangeListener(){
+        /**
+         * 当system bar 设置LOW_PROFILE, HIDE_NAVIGATION, or FULLSCREEN flags，会触发
+         */
+        window.decorView.setOnSystemUiVisibilityChangeListener {
+            visibility ->
+            if ((visibility and View.SYSTEM_UI_FLAG_FULLSCREEN) == 0) {// system bar可见
+                Toast.makeText(applicationContext," system bar 显示出来",Toast.LENGTH_SHORT).show()
+            }else{// system bar不可见
+                Toast.makeText(applicationContext," system bar被隐藏",Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+    /**
+     * 手势处理类
+     */
+    lateinit var  gestureDetector:GestureDetector
+    /**
+     * 添加手势处理
+     */
+    fun setGestureHandler(){
+        //创建一个 gestrue Dector来处理onTouch()中信息。
+       gestureDetector= GestureDetector(this.applicationContext,object :GestureDetector.SimpleOnGestureListener(){
+           override fun onSingleTapUp(e: MotionEvent): Boolean {
+               Log.i(tag,"onSingleTapUp")
+               return super.onSingleTapUp(e)
+           }
+
+           override fun onDown(e: MotionEvent): Boolean {
+               Log.i(tag," onDown ")
+               return true
+           }
+       })
+        gestureDetector.setIsLongpressEnabled(false)
+    }
+
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
+        var result= gestureDetector.onTouchEvent(event)
+        return result
+    }
+    override fun onClick(view: View) {
+         when(view.id){
+             R.id.main_normalStyle->{
+                   SystemUIManager.setNormalStyle(window)
+              }
+              R.id.main_stickyStyle->{
+                  SystemUIManager.setStickyStyle(window)
+              }
+             R.id.main_dimStyle->{
+                 SystemUIManager.setDimStyle(window)
+             }
+         }
+    }
+
+    /**
+     * 当焦点占据时候，隐藏system bar.
+     */
+    override fun onWindowFocusChanged(hasFocus: Boolean) {
+        if (hasFocus){
+             SystemUIManager.setNormalStyle(window)
+        }
+        super.onWindowFocusChanged(hasFocus)
+    }
+}
---
+++ app/src/main/java/com/xingen/systemuidemo/SystemUIManager.kt
@@ -0,0 +1,64 @@
+package com.xingen.systemuidemo
+
+import android.view.View
+import android.view.Window
+
+/**
+ * Created by ${xingen} on 2017/7/27.
+ *
+ * 管理系统UI,显示与隐藏NavigationBar 和StatusBar
+ */
+object SystemUIManager {
+    /**
+     * 隐藏NavigationBar
+     */
+    var action_navigation_bar_hide = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+
+    /**
+     * 全屏，会隐藏StatusBar
+     */
+    var action_full_screen = View.SYSTEM_UI_FLAG_FULLSCREEN
+    /**
+     * 系统4.1或者更高版本，可以让NavigationBar和StatusBar浮在内容上面。
+     */
+    var action_navigation_bar_hide_float = View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+    /**
+     * 与SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION结合使用，确保布局稳定
+     */
+    var action_stable = View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+    /**
+     * 使StatusBar和NavigationBar暗淡显示。
+     */
+    var action_dim = View.SYSTEM_UI_FLAG_LOW_PROFILE
+    /**
+     * 系统4.4 出现，该flag可以让程序进入全屏。
+     */
+    var action_immersive = View.SYSTEM_UI_FLAG_IMMERSIVE
+    /**
+     * 系统4.4 出现，该flag可以让系统bar出现，但是一会儿自动消失。Sticky方式
+     */
+    var aciton_immersive = View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+
+    /**
+     * Sticky风格
+     */
+    fun setStickyStyle(window: Window) {
+        var flag = action_navigation_bar_hide or action_full_screen or action_navigation_bar_hide_float or action_navigation_bar_hide_float or action_stable or aciton_immersive
+        window.decorView.systemUiVisibility = flag
+    }
+    /**
+     * 正常风格
+     */
+    fun setNormalStyle(window: Window){
+        var flag = action_navigation_bar_hide or action_full_screen
+        window.decorView.systemUiVisibility = flag
+    }
+
+    /**
+     * 暗淡风格
+     */
+    fun  setDimStyle(window: Window){
+        var flag = action_dim
+        window.decorView.systemUiVisibility = flag
+    }
+}
